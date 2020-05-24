7265627bc2ba7d2f9450b81ff52395981bbf3574
Martin Helmut Dominguez Alvarez <martinhelmutmh@gmail.com>
2020-05-24 17:49:50 -0500

    Currently on work

--- Calculavirus-ios/Calculavirus-ios/Prueba/Prueba.swift
+++ Calculavirus-ios/Calculavirus-ios/Prueba/Prueba.swift
@@ -4,51 +4,54 @@ import SwiftUI
 
 struct ContentViewPhoto: View {
     
-    @State var image: Image? = nil
+    @State var image: UIImage? = nil
     @State var showCaptureImageView: Bool = false
     
     var body: some View {
         ZStack {
             VStack {
                 Button(action: {
-                  self.showCaptureImageView.toggle()
+                    self.showCaptureImageView.toggle()
                 }) {
                     Text("Choose photos")
                 }
-                image?.resizable()
-                  .frame(width: 250, height: 250)
-                  .clipShape(Circle())
-                  .overlay(Circle().stroke(Color.white, lineWidth: 4))
-                  .shadow(radius: 10)
+                if(image != nil){
+                    Image(uiImage: image!)
+                        .resizable()
+                        .frame(width: 250, height: 250)
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                        .shadow(radius: 10)
+                }
             }
             
             if (showCaptureImageView) {
-                   CaptureImageView(isShown: $showCaptureImageView, image: $image)
-                 }
+                CaptureImageView(isShown: $showCaptureImageView, image: $image)
+            }
         }
     }
 }
 
 struct CaptureImageView {
-  /// MARK: - Properties
-  @Binding var isShown: Bool
-  @Binding var image: Image?
-  
-  func makeCoordinator() -> Coordinator {
-    return Coordinator(isShown: $isShown, image: $image)
-  }
+    /// MARK: - Properties
+    @Binding var isShown: Bool
+    @Binding var image: UIImage?
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(isShown: $isShown, image: $image)
+    }
 }
 
 extension CaptureImageView: UIViewControllerRepresentable {
     func makeUIViewController(context: UIViewControllerRepresentableContext<CaptureImageView>) -> UIImagePickerController {
         let picker = UIImagePickerController()
         picker.delegate = context.coordinator
-       if !UIImagePickerController.isSourceTypeAvailable(.camera){
-           picker.sourceType = .photoLibrary
-       } else {
-           picker.sourceType = .camera
-       }
-       return picker
+        if !UIImagePickerController.isSourceTypeAvailable(.camera){
+            picker.sourceType = .photoLibrary
+        } else {
+            picker.sourceType = .camera
+        }
+        return picker
     }
     
     func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<CaptureImageView>){
@@ -57,21 +60,25 @@ extension CaptureImageView: UIViewControllerRepresentable {
 }
 
 class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-  @Binding var isCoordinatorShown: Bool
-  @Binding var imageInCoordinator: Image?
-  init(isShown: Binding<Bool>, image: Binding<Image?>) {
-    _isCoordinatorShown = isShown
-    _imageInCoordinator = image
-  }
-  func imagePickerController(_ picker: UIImagePickerController,
-                didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-     guard let unwrapImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
-     imageInCoordinator = Image(uiImage: unwrapImage)
-     isCoordinatorShown = false
-  }
-  func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-     isCoordinatorShown = false
-  }
+    @Binding var isCoordinatorShown: Bool
+    @Binding var imageInCoordinator: UIImage?
+    
+    init(isShown: Binding<Bool>, image: Binding<UIImage?>) {
+        _isCoordinatorShown = isShown
+        _imageInCoordinator = image
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let unwrapImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        
+        //    Observa
+        imageInCoordinator = unwrapImage
+        isCoordinatorShown = false
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        isCoordinatorShown = false
+    }
 }
 
 struct ContentViewPhoto_Previews: PreviewProvider {
--- Calculavirus-ios/Calculavirus-ios/UI/Lugares/LugaresForm.swift
+++ Calculavirus-ios/Calculavirus-ios/UI/Lugares/LugaresForm.swift
@@ -14,6 +14,12 @@ struct  ServerMessage : Decodable {
     let status, message : String?
 }
 
+struct LugarSend {
+    let nombre : String
+    let descripcion : String
+    let image : NSData?
+}
+
 class HttpAuth: ObservableObject {
     var didChange = PassthroughSubject<HttpAuth, Never>()
     
@@ -23,33 +29,81 @@ class HttpAuth: ObservableObject {
         }
     }
     
-    func checkDetails(nombre: String, descripcion: String) {
+    func checkDetails(nombre: String, descripcion: String, image: UIImage?) {
+        let base64String: String
+        let body:[String : Any]
+        
         let urlString = "http://127.0.0.1:8000/lugares/"
         guard let url = URL(string: urlString) else {return}
         
-        let body: [String : String] = [
-            "nombre" : nombre,
-            "descripcion" : descripcion
-        ]
+        
+        let dataImage = image?.pngData()
+        
+        if dataImage == nil {
+            
+//            body = [
+//                       "nombre" : nombre,
+//                       "descripcion" : descripcion
+//                   ]
+            
+            
+        }else{
+             base64String = dataImage!.base64EncodedString(options: .lineLength64Characters)
+                
+            body = [
+                "nombre" : nombre,
+                "descripcion" : descripcion,
+                "image" : base64String
+            ]
+        }
+        
+        print(dataImage)
+        
+        
+        
+        print(body)
         
         let finalBody = try! JSONSerialization.data(withJSONObject: body)
+        guard let uploadData = try? JSONEncoder().encode(body) else {
+            return
+        }
         
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.addValue("Basic bm9yY286bm9yY29ub3Jjbw==", forHTTPHeaderField: "Authorization")
-        request.httpBody = finalBody
-        
+//        request.httpBody = finalBody
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         
-        URLSession.shared.dataTask(with: request){(data, response, error) in
-            
-            guard let data = data else { return }
-            
-            let finalData = try! JSONDecoder().decode(ServerMessage.self, from: data)
-            
-            print(finalData)
-            
-        }.resume()
+//        URLSession.shared.dataTask(with: request){(data, response, error) in
+//
+//            guard let data = data else { return }
+//
+//            let finalData = try! JSONDecoder().decode(ServerMessage.self, from: data)
+//
+//            print(finalData)
+//            print(response)
+//            print(error)
+//
+//        }.resume()
+        
+        let task = URLSession.shared.uploadTask(with: request, from: uploadData) { data, response, error in
+            if let error = error {
+                print ("error: \(error)")
+                return
+            }
+            guard let response = response as? HTTPURLResponse,
+                (200...299).contains(response.statusCode) else {
+                print ("server error")
+                return
+            }
+            if let mimeType = response.mimeType,
+                mimeType == "application/json",
+                let data = data,
+                let dataString = String(data: data, encoding: .utf8) {
+                print ("got data: \(dataString)")
+            }
+        }
+        task.resume()
         
     }
 }
@@ -58,15 +112,15 @@ struct LugaresForm: View {
     
     @State private var nombre = ""
     @State private var descripcion = ""
-    @State private var image: Image? = nil
+    @State private var image: UIImage? = nil
     @State private var showCaptureImageView: Bool = false
     
     var manager = HttpAuth()
     
     var body: some View {
         NavigationView{
-          
             Form{
+                
                 TextField("Nombre", text: $nombre)
                 TextField("Descripcion", text: $descripcion)
                 
@@ -75,35 +129,38 @@ struct LugaresForm: View {
                         Button(action: {
                             self.showCaptureImageView.toggle()
                         }) {
-                            Text("Choose photos")
+                            Text("Imagen (opcional)")
                         }
-                        
+                            
                         .sheet(isPresented: $showCaptureImageView) {
                             CaptureImageView(isShown: self.$showCaptureImageView, image: self.$image)
                         }
-
-                        image?
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
+                        if(image != nil){
+                            Image(uiImage: image ?? UIImage())
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                        }
+                        
                     }
-
-//                    if (showCaptureImageView) {
-//                        CaptureImageView(isShown: $showCaptureImageView, image: $image)
-//                    }
+                    
+                    //                    if (showCaptureImageView) {
+                    //                        CaptureImageView(isShown: $showCaptureImageView, image: $image)
+                    //                    }
                 }
                 
                 
                 Section{
                     Button("Guardar", action: {
-                       print(self.nombre)
-                       print(self.descripcion)
-                        self.manager.checkDetails(nombre: self.nombre, descripcion: self.descripcion)
-                   })
+                        print(self.nombre)
+                        print(self.descripcion)
+                        print(self.image)
+                        self.manager.checkDetails(nombre: self.nombre, descripcion: self.descripcion, image: self.image ?? UIImage())
+                    })
                 }
                 
             }
                 
-        .navigationBarTitle("Registrar Lugar")
+            .navigationBarTitle("Registrar Lugar")
         }
     }
 }
